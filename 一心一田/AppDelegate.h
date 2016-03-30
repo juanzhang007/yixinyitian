a027071fa4d7e1db3dcfc0574eeadb4036cc3eaf
zhangjuan <1454252427@qq.com>
2016-03-30 22:22:39 +0800

    11gt

--- 一心一田/AppDelegate.h
+++ 一心一田/AppDelegate.h
@@ -2,8 +2,8 @@
 //  AppDelegate.h
 //  一心一田
 //
-//  Created by user on 16/3/30.
-//  Copyright © 2016年 user. All rights reserved.
+//  Created by xipin on 16/3/7.
+//  Copyright © 2016年 xipin. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- 一心一田/main.m
+++ 一心一田/main.m
@@ -2,12 +2,13 @@
 //  main.m
 //  一心一田
 //
-//  Created by user on 16/3/30.
-//  Copyright © 2016年 user. All rights reserved.
+//  Created by xipin on 16/3/7.
+//  Copyright © 2016年 xipin. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
